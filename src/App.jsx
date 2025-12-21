a5d303f5d002774e9ce604bfa0026d36ccbc739c
Tosif2230 <techie.tosif@gmail.com>
2025-12-21 10:50:52 +0530

    Routing Configure

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Header from "./components/Header";
 import ProductList from "./components/ProductList";
 import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Header />
-      <ProductList/>
+      <Outlet/>
       <Footer/>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from "./App.jsx"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ProductList from './components/ProductList.jsx'
+
+
+const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <App/>,
+      children: [{
+        path: "/",
+        element: <ProductList/>
+      }
+    ]
+    }
+
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={routes}/>
 )
